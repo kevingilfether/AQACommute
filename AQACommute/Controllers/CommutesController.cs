6d8cde4604661b1f4dbb99b31b65dda5ca164c9e
briandyko <briandyko@gmail.com>
2017-11-29 10:38:40 -0500

    vehicle and commute models imported, controllers scaffolded

---
+++ AQACommute/Controllers/CommutesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AQACommute.Models;
+
+namespace AQACommute.Controllers
+{
+    public class CommutesController : Controller
+    {
+        private AQAMvpEntities db = new AQAMvpEntities();
+
+        // GET: Commutes
+        public ActionResult Index()
+        {
+            var commutes = db.Commutes.Include(c => c.Vehicle);
+            return View(commutes.ToList());
+        }
+
+        // GET: Commutes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Commute commute = db.Commutes.Find(id);
+            if (commute == null)
+            {
+                return HttpNotFound();
+            }
+            return View(commute);
+        }
+
+        // GET: Commutes/Create
+        public ActionResult Create()
+        {
+            ViewBag.VehicleID = new SelectList(db.Vehicles, "VehicleID", "Make");
+            return View();
+        }
+
+        // POST: Commutes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CommuteID,DistanceTraveled,CommuteTimeSpent,CommuteTimeEnd,C02Footprint,VehicleID")] Commute commute)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Commutes.Add(commute);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.VehicleID = new SelectList(db.Vehicles, "VehicleID", "Make", commute.VehicleID);
+            return View(commute);
+        }
+
+        // GET: Commutes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Commute commute = db.Commutes.Find(id);
+            if (commute == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.VehicleID = new SelectList(db.Vehicles, "VehicleID", "Make", commute.VehicleID);
+            return View(commute);
+        }
+
+        // POST: Commutes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CommuteID,DistanceTraveled,CommuteTimeSpent,CommuteTimeEnd,C02Footprint,VehicleID")] Commute commute)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(commute).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.VehicleID = new SelectList(db.Vehicles, "VehicleID", "Make", commute.VehicleID);
+            return View(commute);
+        }
+
+        // GET: Commutes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Commute commute = db.Commutes.Find(id);
+            if (commute == null)
+            {
+                return HttpNotFound();
+            }
+            return View(commute);
+        }
+
+        // POST: Commutes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Commute commute = db.Commutes.Find(id);
+            db.Commutes.Remove(commute);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
