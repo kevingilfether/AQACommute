016bae522cb50796cc2994ef3f4cba2c745b9ae5
kevingilfether <kevingilfether@gmail.com>
2017-12-07 15:12:16 -0500

    Made TransportCommuteViewModel

---
+++ AQACommute/Models/TransportCommuteViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AQACommute.Models
+{
+    public class TransportCommuteViewModel
+    {
+        public Commute commute { get; set; }
+        public TransportMethod transportMethod { get; set; }
+
+    }
+}
