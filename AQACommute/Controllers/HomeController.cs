2565ef7c2b99ac4fc454ba6c0631e16c84c5969c
royernst <roy.e31@hotmail.com>
2017-12-11 14:50:40 -0500

    Added landing page redirect

--- AQACommute/Controllers/HomeController.cs
+++ AQACommute/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace AQACommute.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("LandingPage");
         }
 
         public ActionResult ThePoint()
